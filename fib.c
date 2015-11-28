e5b24de572b360e7d0de531b6bf9b2148246ee05
Trevor <trevenrawr@gmail.com>
2015-11-28 15:16:11 -0600

    Adding the actual C file.

---
+++ fib.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+
+public int fib(int n) {
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+	int n;
+	long f;
+
+	printf( "Enter fib number:" );
+	scanf( "%d", &n );
+	&f = fib(n);
+	printf( "Fib of %d is %d. \n", n, f);
+	getchar();
+	return 0;
+}
